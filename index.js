ccf741e747f2b746807d88d792154af9349f3084
SoNomad <nurmuhammad16203@gmail.com>
2022-09-05 17:45:36 +0300

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+import os from 'os';
+import welcome from './welcome.js'
+
+console.log(os.homedir())
+console.log(os.hostname())
+console.log(os.version())
+console.log(os.type())
+
+welcome()
---
+++ welcome.js
@@ -0,0 +1,3 @@
+import os from 'os'
+
+export default function () {console.log(`Привет, юзер. Вижу ты зашел с ${os.type()}`)}
